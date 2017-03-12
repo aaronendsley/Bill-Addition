a1217a28b9742a1d754a1b88a14eeb59c6a2257d
Aaron Endsley <aaronendsley@gmail.com>
2017-03-12 17:26:00 -0500

    update

--- src/app/bill-form/bill-form.component.ts
+++ src/app/bill-form/bill-form.component.ts
@@ -1,31 +1,35 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import {
+    Component,
+    Output,
+    EventEmitter
+} from '@angular/core';
 
 
 @Component({
-  selector: 'app-bill-form',
-  templateUrl: './bill-form.component.html',
-  styleUrls: ['./bill-form.component.css']
+    selector: 'app-bill-form',
+    templateUrl: './bill-form.component.html',
+    styleUrls: ['./bill-form.component.css']
 })
-export class BillFormComponent{
-@Output() submit = new EventEmitter();
+export class BillFormComponent {
+    @Output() submit = new EventEmitter();
 
 
-submitTheBills(Input1: HTMLInputElement, Input2: HTMLInputElement){
-  const amountFilteredForCommas: string = this.filterForCommas(Input2.value);
-  const billArray: string[] = [Input1.value, amountFilteredForCommas];
-  this.submit.emit(billArray);
-  this.resetTheValues(Input1, Input2);
-};
-  resetTheValues(resetTitle, resetAmount): void {
-            resetTitle.value = '';
-            resetAmount.value = '';
-        }
+    submitTheBills(Input1: HTMLInputElement, Input2: HTMLInputElement) {
+        const amountFilteredForCommas: string = this.filterForCommas(Input2.value);
+        const billArray: string[] = [Input1.value, amountFilteredForCommas];
+        this.submit.emit(billArray);
+        this.resetTheValues(Input1, Input2);
+    };
+    resetTheValues(resetTitle, resetAmount): void {
+        resetTitle.value = '';
+        resetAmount.value = '';
+    }
 
-filterForCommas(input: string){
-    const arrayWithCommas = input.split('');
-    const stringWithNoCommas = arrayWithCommas.filter(function(value){
-        return value !== ',';
-    }).join('');
-    return stringWithNoCommas;
-  }
+    filterForCommas(input: string) {
+        const arrayWithCommas = input.split('');
+        const stringWithNoCommas = arrayWithCommas.filter(function (value) {
+            return value !== ',';
+        }).join('');
+        return stringWithNoCommas;
+    }
 }
